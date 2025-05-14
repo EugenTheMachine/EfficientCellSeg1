0ea049464ea9ecd15997849737106962a6140b49
eugen <lazoskal24@gmail.com>
2025-05-14 18:54:56 +0300

    -

--- seg_any/predictor.py
+++ seg_any/predictor.py
@@ -85,8 +85,9 @@ class SamPredictor:
 
         self.original_size = original_image_size
         self.input_size = tuple(transformed_image.shape[-2:])
-        input_image = self.model.preprocess(transformed_image)
-        self.features = self.model.image_encoder(input_image)
+        # input_image = self.model.preprocess(transformed_image)
+        # self.features = self.model.image_encoder(input_image)
+        self.features = self.model.encoder_image_embeddings([input_image])[0]
         self.is_image_set = True
 
     def predict(
